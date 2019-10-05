02ff325f653f9ad5e22d74a2a1da10c359b7a8db
Arthur <arthurcs426@gmail.com>
2019-10-05 09:26:08 -0700

    working on redux store, then create a simple front end

---
+++ src/store.js
@@ -0,0 +1,48 @@
+import { createStore, combineReduceres } from 'redux';
+import axios from 'axios';
+import thunk from 'redux-thunk';
+
+const SET_SCHOOLS = 'SET_SCHOOLS';
+
+const SET_STUDENTS = 'SET_STUDENTS';
+
+const studentsReducer = (state = [], action) => {
+  switch(action.type) {
+    case SET_STUDENTS:
+      return action.student;
+  }
+  return state;
+};
+
+const schoolsReducer = (state = [], action) => {
+  switch(action.type) {
+    case SET_SCHOOLS:
+      return action.schools;
+  }
+  return state;
+};
+
+const reducer = combineReducers({
+  students: studentsReducer,
+  schools: schoolsReducer
+});
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+// ACTION CREATORS
+const setStudents = (students) => ({ type: SET_STUDENTS, students });
+
+const setSchools = (schools) => ({ type: SET_SCHOOLS, schools });
+
+// METHODS
+const API = '/api/';
+
+const getStudents = () => {
+  return async (dispatch) => {
+    const students = (await axios.get(`${API}/students`)).data;
+    dispatch(setStudents(students));
+  };
+};
+
+export default store;
+
